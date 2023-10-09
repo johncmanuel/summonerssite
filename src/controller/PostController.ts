50d805edc8b877b037ecc194134e645962616ce2
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-10-09 10:51:42 -0700

    update controllers for entities

---
+++ src/controller/PostController.ts
@@ -0,0 +1,67 @@
+import { AppDataSource } from "../data-source";
+import { NextFunction, Request, Response } from "express";
+import { Post } from "../entity/Post";
+import { User } from "../entity/User";
+
+export class PostController {
+	private postRepository = AppDataSource.getRepository(Post);
+	private userRepository = AppDataSource.getRepository(User);
+
+	// Get all posts from a particular user
+	async all(request: Request, response: Response, next: NextFunction) {
+		const id = parseInt(request.params.id);
+		const user = await this.userRepository.findOne({
+			where: { id },
+		});
+		const posts = this.postRepository.find({
+			where: { user },
+		});
+		return posts;
+	}
+
+	async one(request: Request, response: Response, next: NextFunction) {
+		const id = parseInt(request.params.id);
+		const postId = parseInt(request.params.postId);
+		const user = await this.userRepository.findOne({
+			where: { id },
+		});
+		const post = this.postRepository.findOne({
+			where: { user, id: postId },
+		});
+		return post;
+	}
+
+	async save(request: Request, response: Response, next: NextFunction) {
+		const { title, description, datePosted } = request.body;
+		const id = parseInt(request.params.id);
+		const user = await this.userRepository.findOne({
+			where: { id },
+		});
+		const post = Object.assign(new Post(), {
+			title,
+			description,
+			datePosted,
+			user,
+		});
+		return this.postRepository.save(post);
+	}
+
+	async remove(request: Request, response: Response, next: NextFunction) {
+		const id = parseInt(request.params.id);
+		const postId = parseInt(request.params.postId);
+		const user = await this.userRepository.findOne({
+			where: { id },
+		});
+		let postToRemove = await this.postRepository.findOneBy({
+			id: postId,
+			user,
+		});
+
+		if (!postToRemove) {
+			return "this post not exist";
+		}
+
+		await this.postRepository.remove(postToRemove);
+		return "post has been removed";
+	}
+}

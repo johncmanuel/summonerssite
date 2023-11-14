7645e439fcac5b66f2d489d5766fe8d09e53b1ba
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2023-11-13 20:47:32 -0800

    use env variables for database

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,17 +3,29 @@ import { DataSource } from "typeorm";
 import { User } from "./entity/User";
 import { Post } from "./entity/Post";
 import { Comment } from "./entity/Comment";
+import {
+	DB_HOST,
+	DB_PORT,
+	DB_PASSWORD,
+	DB_USERNAME,
+	DB_DATABASE,
+} from "../env";
+
+console.log(DB_DATABASE);
 
 export const AppDataSource = new DataSource({
 	type: "mysql",
-	host: "localhost",
-	port: 3306,
-	username: "root",
-	password: "password",
-	database: "summonerssite",
-	synchronize: true,
+	port: DB_PORT,
+	host: DB_HOST,
+	username: DB_USERNAME,
+	password: DB_PASSWORD,
+	database: DB_DATABASE,
+	synchronize: process.env.NODE_ENV === "development",
 	logging: false,
 	entities: [User, Post, Comment],
 	migrations: [],
 	subscribers: [],
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
